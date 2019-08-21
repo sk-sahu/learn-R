7939dc08f4651a11d9b7b113d5c238aed5f317bb
sk-sahu <sk-sahu@github.com>
2019-08-22 00:03:35 +0530

    fold a block

---
+++ docs/update.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+# print_usage function
+print_usage() {
+  printf "Usage: bash update.sh -m 'message for commit' \n\n"
+  printf "    -m: message for commit \n\n"
+}
+
+# Arguments default
+msg_flag='no_msg'
+
+# get arguments
+while getopts 'm:' flag; do
+  case "${flag}" in
+  m) msg_flag="${OPTARG}";;
+  *)
+    print_usage
+    exit 1
+    ;;
+  esac
+done
+
+#echo $msg_flag
+
+Rscript ./render_rmd2html.R
+
+git add .
+git commit -m $msg_flag
+git push
