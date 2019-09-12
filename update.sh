10fb8692b67def8c3fc11843ab531a64c7a5f562
sksahucode <sksahucode@bitbucket.com>
2019-09-12 12:24:16 +0530

    start n end pannel added

--- update.sh
+++ update.sh
@@ -5,38 +5,25 @@
 # to some html file and push updates to git.
 #------------------------------------------------------------
 
-# print_usage function
-print_usage() {
-  printf "Usage: bash update.sh -m 'message for commit' \n\n"
-  printf "    -m: message for commit \n\n"
-}
+# Usage: bash update.sh 'message for commit'
 
 # Arguments default
 msg_flag='no_msg'
-
-# get arguments
-while getopts 'm:' flag; do
-  case "${flag}" in
-  m) msg_flag="${OPTARG}";;
-  *)
-    print_usage
-    exit 1
-    ;;
-  esac
-done
+msg_flag=$1
 
 echo "============= Rendering =============="
 # render all the Rmd files to html in /docs
+rm -rf docs/*
 Rscript ./render_rmd2html.R
 
 echo "========= Encrypting test files ========="
 # https://github.com/robinmoisson/staticrypt
-rm docs/*_encrypted.html
+#rm docs/*_encrypted.html
 find . -type f -name "test*.html" -exec staticrypt {} go -f style/pwd_templet.html -t "Take a Test" \;
 
 echo "========= Updates to Github ==========="
 # push the updates
 git config --global push.default simple
 git add --all
-git commit -a -m $msg_flag
+git commit -m $msg_flag
 git push
