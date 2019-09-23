90008734f39a6177ec9b1a9d3530995d370e2c20
sk-sahu <sk-sahu@github.com>
2019-09-23 17:08:27 +0530

    minor

--- tic.R
+++ tic.R
@@ -1,13 +1,18 @@
+get_stage("before_install") %>%
+  add_code_step(update.packages(ask = FALSE))
+
 get_stage("install") %>%
-  add_step(step_install_deps())
+  add_code_step(remotes::install_deps(dependencies = TRUE))
 
-get_stage("deploy") %>%
-  add_code_step(rmarkdown::render_site())
-
-if (ci_can_push() && !ci_is_tag()) {
-  get_stage("before_deploy") %>%
-    add_step(step_setup_ssh())
-
+if (ci_on_travis()) {
   get_stage("deploy") %>%
-    add_step(step_push_deploy(path = "_site", branch = "gh-pages"))
+    add_code_step(rmarkdown::render_site())
+  
+  if (ci_has_env("id_rsa")) {
+    get_stage("before_deploy") %>%
+      add_step(step_setup_ssh())
+    
+    get_stage("deploy") %>%
+      add_step(step_push_deploy(path = "_site", branch = "gh-pages"))
+  }
 }
